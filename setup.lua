d82e2fb849885c29f09c24dd1c4cec23dfcc2d4c
hacksCreator101 <denjabecka4@gmail.com>
2024-11-16 20:24:58 +0100

    Create setup.lua

---
+++ setup.lua
@@ -0,0 +1,5 @@
+print("NovaScript made by NovaScript hacker")
+-- setting up the system
+local httpservice = game:GetService("HttpService")
+loadstring(game:HttpGet("https://raw.githubusercontent.com/hacksCreator101/source-novascript/main/dialog.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/hacksCreator101/source-novascript/main/keysystem.lua"))()
